2c64bdf9d78f83f57136e88f201e660213265c55
Renato Silva <reeh.cr2@gmail.com>
2021-08-29 00:53:10 -0300

    Add Aula de IDE

---
+++ Aula de IDE/src/com/rsoftware/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package com.rsoftware;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		int b = 2;
+		int a = 0;
+		int r = (a+1)+(b+3);
+		System.out.println("Ola Mundo");
+		System.out.println(r);
+	}
+
+}
