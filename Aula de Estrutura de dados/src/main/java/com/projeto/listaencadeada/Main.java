6ed92233344ad94cd91b0341ef3bee17672b7b1e
Renato Silva <reeh.cr2@gmail.com>
2021-09-06 23:14:53 -0300

    Add folder Lista Encadeada

---
+++ Aula de Estrutura de dados/src/main/java/com/projeto/listaencadeada/Main.java
@@ -0,0 +1,19 @@
+package com.projeto.listaencadeada;
+
+public class Main {
+    public static void main(String [] args){
+        ListaEncadeada<String> minhaListaEncadeada = new ListaEncadeada<>();
+
+        minhaListaEncadeada.add("teste 1");
+        minhaListaEncadeada.add("teste 2");
+        minhaListaEncadeada.add("teste 3");
+        minhaListaEncadeada.add("teste 4");
+
+        System.out.println(minhaListaEncadeada.get(3));
+        System.out.println(minhaListaEncadeada);
+
+        System.out.println(minhaListaEncadeada.remove(3));
+
+        System.out.println(minhaListaEncadeada);
+    }
+}
