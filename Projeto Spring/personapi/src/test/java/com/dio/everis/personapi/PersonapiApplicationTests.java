a674070871a2904906cac460e100de463c76805c
Renato Silva <reeh.cr2@gmail.com>
2021-09-10 19:38:20 -0300

    Add folder Projeto Spring

---
+++ Projeto Spring/personapi/src/test/java/com/dio/everis/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.everis.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
